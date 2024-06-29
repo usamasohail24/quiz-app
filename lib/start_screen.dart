9cb8a8e9483a1405458fddbbeb54429dab1e4144
Usama Sohail <usamasohail24>
2024-06-30 03:48:47 +0500

    Initial Commit

---
+++ lib/start_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Opacity(
+            opacity: 0.5,
+            child: Image.asset(
+              'assets/images/quiz-logo.png',
+              width: 200,
+            ),
+          ),
+          const SizedBox(height: 80),
+          const Text(
+            'Welcome to Quiz App',
+            style: TextStyle(
+              color: Colors.amberAccent,
+              fontWeight: FontWeight.bold,
+              fontSize: 18,
+            ),
+          ),
+          const SizedBox(height: 50),
+          OutlinedButton.icon(
+            onPressed: () 
+            {
+
+            },
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.blue,
+            ),
+            icon: const Icon(
+              Icons.arrow_right_alt,
+              color: Color.fromARGB(255, 53, 157, 243),
+            ),
+            label: const Text(
+              'Start Quiz',
+              style: TextStyle(
+                color: Colors.amberAccent,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
